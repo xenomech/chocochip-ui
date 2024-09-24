cdacaaaeb9f547f1e59e00af55a69845ba906cfb
xenomech <gokulsmenon227@gmail.com>
2024-09-25 03:00:25 +0530

    fix: update import statement for terser plugin in rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const config = [
   {
